50627da2ea2b6ad74ca3a27724912986bbc54576
manavisharma14 <manavisharma14@gmail.com>
2026-05-06 18:52:35 -0500

    displaying no of tabs + name

--- home.js
+++ home.js
@@ -1,18 +1,14 @@
 chrome.tabs.query({}, (tabs) => {
-
-    //update tab count
     const countElement = document.getElementById("tab-count");
 
     countElement.textContent = `You have ${tabs.length} tabs open`;
 
     const tabList = document.getElementById("tab-list");
 
-    // loop through tabs
     tabs.forEach((tab) => {
-        // create list item
         const listItem = document.createElement("li");
         // set tab title
-        listItem.textContent = tab.createContent("li");
+        listItem.textContent = tab.title;
 
         // add item to list
         tabList.appendChild(listItem);
